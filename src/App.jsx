287a0e49cc23fb047d6a4f79ae65e2f2110cb1f5
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-19 23:36:29 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import styles from "./style";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Business from "./components/Business";
+import Button from "./components/Button";
+import Billing from "./components/billing";
+import CardDeal from "./components/CardDeal";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer"
+import Getstarted from "./components/Getstarted"
+import Stats from "./components/Stats"
+import Testimonials from "./components/Testimonials"
+import FeedbackCard from "./components/FeedbackCard";
+import Clients from "./components/Clients"
 const App =()=>(
 
     <div className="bg-primary w-full overflow-hidden">
   <div className={`${styles.paddingX} ${styles.flexCenter}`} >
     <div className={`${styles.boxWidth}`} >
-navbar
+<Navbar/>
     </div>
   </div>
   <div className={`bg-primary ${styles.flexStart}`} >  
     <div className={`${styles.boxWidth}`} >
-hero
+<Hero/>
     </div>
 
   </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { useState } from "react"
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
+    const [toogle, settoogle] = useState(false)
   return (
-    <div>
-      
-    </div>
+  <nav className="w-full flex py-6 justify-between items-center navbar" >
+<img src="src/assets/logo.png" alt="" className="w-[120px] h-[120px] "/>
+<ul className="list-none sm:flex hidden justify-end items-center flex-1" >
+{navLinks.map((nav,index)=>(
+    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-white ${index===navLinks.length-1? 'mr-0':'mr-10'}  text-[16px] `} >
+        <a href={`#${nav.id}`}>
+            {nav.title}
+        </a>
+    </li>
+))}
+</ul>
+<div className="sm:hidden items-center flex flex-1 justify-end" >
+<img src={toogle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={()=>settoogle((prev)=>!toogle )} />
+
+        <div
+          className={`${
+            !toogle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+            <ul className="list-none flex-col flex  justify-end items-center flex-1" >
+{navLinks.map((nav,index)=>(
+    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-white ${index===navLinks.length-1? 'mr-0':'mb-4'}  text-[16px] `} >
+        <a href={`#${nav.id}`}>
+            {nav.title}
+        </a>
+    </li>
+))}
+</ul>
+            
+
+
+        </div>
+</div>
+  </nav>
   )
 }
 
