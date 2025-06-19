f26420c9e26f1475c2031fcb1714288abf712e7c
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-20 00:14:15 +0530

    Hero section added

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Getstarted = () => {
   return (
-    <div>
+    <div className='text-white'>
+      fghndfsgjkl
       
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import styles from "../style"
+import discountNew from "../assets/discount.png";
+import { discount,robot } from "../assets"
+import Getstarted from "./components/Getstarted"
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+    
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `} >
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 overflow-hidden`} >
+        <div className="flex flex-row item-center py-[6px] px-4 overflow-hidden bg-discount-gradient rounded-[10px] mb-2" >
+         <img src={discountNew} alt="discount" className="w-[30px] h-[30px] object-contain" />
+<p className={`${styles.paragraph} ml-2`}>
+  <span className="text-white" >50%</span> Discount For {" "}
+<span className="text-white" > One Month</span> Account
+</p>
+        </div>
+        <div className="flex flex-row items-center w-full justify-between" >
+<h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss-leading-[100px] leading-[75px] text-white">
+      The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "} <br />
+            payment method
+</h1>
+<div className="ss=flex hidden md:mr-4 mr-0" > 
+<Getstarted/>
+</div>
+        </div>
+      </div>
+ </section>
   )
 }
 
