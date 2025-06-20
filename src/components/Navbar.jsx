761741d1f76c972a356a1a4159e4cc36422c7a67
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-20 16:10:26 +0530

    Navbar hover effect added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,21 @@ const Navbar = () => {
   <nav className="w-full flex py-6 justify-between items-center navbar" >
 <img src="src/assets/logo.png" alt="" className="w-[120px] h-[120px] "/>
 <ul className="list-none sm:flex hidden justify-end items-center flex-1" >
-{navLinks.map((nav,index)=>(
-    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-white ${index===navLinks.length-1? 'mr-0':'mr-10'}  text-[16px] `} >
-        <a href={`#${nav.id}`}>
-            {nav.title}
-        </a>
-    </li>
+{navLinks.map((nav, index) => (
+  <li
+    key={nav.id}
+    className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
+      index === navLinks.length - 1 ? "mr-0" : "mr-10"
+    } relative group transition-all duration-300`}
+  >
+    <a
+      href={`#${nav.id}`}
+      className="group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-[radial-gradient(circle_at_30%_30%,_#00CFFF,_#D200FF_70%)]"
+    >
+      {nav.title}
+    </a>
+    <span className="absolute left-0 bottom-[-2px] w-0 h-[2px] bg-[radial-gradient(circle_at_30%_30%,_#00CFFF,_#D200FF_70%)] transition-all duration-300 group-hover:w-full"></span>
+  </li>
 ))}
 </ul>
 <div className="sm:hidden items-center flex flex-1 justify-end" >
