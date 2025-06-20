2baa95303dc1d793448dada894032ae427f7168e
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-20 18:44:51 +0530

    Bussiness section added

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,19 +1,46 @@
-import { features } from "../constants"
-import styles, {layout} from "../style"
-import Button from "./Button"
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const Featurecard = ({ icon, title, content, index }) => (
+  <div className="flex flex-row p-6 rounded-[20px] feature-card">
+    <div className="w-[64px] h-[64px] rounded-full bg-dimBlue flex items-center justify-center">
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
 const Business = () => {
   return (
-    <section className={`${layout.section}`} id="features" >
-<div className={`${layout.sectionInfo}`}>
-<h2 className={`${styles.heading2}`}> You focus on the future, <br className="sm:block hidden" /> we’ll power the system.</h2>
-<p className={`${styles.paragraph} max-w-[470px] mt-5`}>  Whether you’re scaling fast or just getting started, our platform automates transactions, boosts rewards, and secures your flow — all in real time.</p>
-<Button
-styles="mt-10"
-/>
-</div>
-    </section>
-  )
-}
+    <section className={`${layout.section}`} id="features">
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          You focus on the future, <br className="sm:block hidden" /> we’ll
+          power the system.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Whether you’re scaling fast or just getting started, our platform
+          automates transactions, boosts rewards, and secures your flow — all
+          in real time.
+        </p>
+        <Button styles="mt-10 " />
+      </div>
 
-export default Business
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <Featurecard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Business;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
-
-
-const Button = ({styles}) => {
+const Button = ({ styles }) => {
   return (
-<button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`} >
-get started 
-</button>
-  )
-}
+    <button
+      type="button"
+      className={`py-3 px-9 bg-blue-gradient font-poppins font-medium text-[18px] rounded-lg text-primary outline-none 
+      transition-all duration-300 ease-in-out hover:brightness-110 hover:scale-105 ${styles}`}
+    >
+      Unleash Access
+    </button>
+  );
+};
 
-export default Button
+export default Button;
