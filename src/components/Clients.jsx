01571c3e60e36028284390e2b05d0522ae30dffc
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-20 19:33:15 +0530

    Clients component created

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../style"
 
 const Clients = () => {
   return (
-    <div>
-      
-    </div>
+<section className={`${styles.flexCenter} my-4`}>
+  <div className={`${styles.flexCenter} flex-wrap w-full`}>
+    {clients.map((client) => (
+      <div
+        key={client.id}
+        className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5 
+        transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-125`}
+      >
+        <img
+          src={client.logo}
+          alt="client_logo"
+          className="sm:w-[192px] w-[100px] object-contain"
+        />
+      </div>
+    ))}
+  </div>
+</section>
+
   )
 }
 
