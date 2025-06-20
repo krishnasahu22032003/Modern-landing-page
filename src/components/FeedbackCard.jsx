564aa7f15aaf7a84881ec75716171100de23d06f
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-20 19:29:48 +0530

    Testimonial component added

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -3,7 +3,7 @@ import { quotes } from "../assets"
 const FeedbackCard = ({content,name,title,img }) => {
   return (
     <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-0 feedback-card'>
-      <img src={quotes} alt="double_quotes" className="w-42px h-27px object-contain" />
+    <img src={quotes} alt="double_quotes" className="w-[42px] h-[27px] object-contain" />
       <p className="font-poppins my-10 leading-[32px] text-white text-[18px] font-normal">{content}</p>
       <div className="flex flex-row" >
 <img src={img} alt="name" className="w-[48-px] h-[48px] rounded-full" />
